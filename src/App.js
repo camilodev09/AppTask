b6929d042b577cd68298a336d302009c2066a7e1
camilodev09 <camilobau2021@gmail.comm>
2023-07-11 12:08:10 -0500

    changeslogoletra

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import Category from "./components/categories/Category";
 import Footer from "./components/Footer";
 import InputForm from "./components/InputForm";
+import logoletra from './assets/logoletra.svg';
 
 function App() {
   const todosItem = useSelector((state) => state.todos.todosList);
@@ -11,7 +12,13 @@ function App() {
       className="w-full min-h-screen pt-4 font-bodyFont bg-gradient-to-t from-purple-800 blue-600 to-purple-300 text-white px-4 flex flex-col gap-10 justify-center items-center rounded-xl
        "
     >
-      <h1 className="text-bodyColor font-semibold font">APP TASK</h1>
+     <div className="w-[350px] flex">
+     <img src={logoletra} alt="apptasker" className="w-full" />
+
+     </div>
+
+     
+    
       {todosItem.length > 0 ? <Category /> : ""}
       <div className="w-full lgl:w-[850px] h-full  bg-bodyColor p-4 lgl:p-10 flex flex-col gap-10 rounded-xl">
         <InputForm />
