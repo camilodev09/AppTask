b0c855ecc9fefb5322fc0ebedcef353e7286873d
camilodev09 <camilobau2021@gmail.com>
2023-07-30 12:41:14 -0500

    go ahead

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import {  FaGlobe , FaFacebook, FaTiktok, } from "react-icons/fa";
 import {  BsWhatsapp } from "react-icons/bs";
 
 
+
 const Footer = () => {
   return (
     <div className="w-full bg-bodyColor">
@@ -12,33 +13,33 @@ const Footer = () => {
       </p>
       <div className="flex justify-center gap-2 mt-3">
      
-            <Link href='https://www.facebook.com/profile.php?id=100065758831949' >
-            <span className="bannerIcon dark:text-gray-100  text-gray-200">
+            <a href='https://www.facebook.com/profile.php?id=100065758831949' >
+            <span className="bannerIcon dark:text-gray-100  text-gray-500">
               <FaFacebook size={30} />
             </span>
-            </Link>
+            </a>
             
-            <Link href="https://www.tiktok.com/@creazone.pe">
-            <span className="bannerIcon dark:text-gray-100  text-gray-200 ">
+            <a href="https://www.tiktok.com/@creazone.pe">
+            <span className="bannerIcon dark:text-gray-100  text-gray-500 ">
               <FaTiktok size={30} />
             </span>
-            </Link>
+            </a>
 
-             <Link href="https://api.whatsapp.com/send?phone=51966301171&text=Deseo%20más%20información.%20Estoy%20interesado.">
-            <span className="bannerIcon dark:text-gray-100  text-gray-200">
+             <a href="https://api.whatsapp.com/send?phone=51966301171&text=Deseo%20más%20información.%20Estoy%20interesado.">
+            <span className="bannerIcon dark:text-gray-100  text-gray-500">
               <BsWhatsapp size={30} />
             </span>
-            </Link>
+            </a>
 
-        <Link
+        <a
           href="https://www.creazone.pe"
           target="_blank"
           rel="noreferrer"
         >
            <span className="footerIcon">
-             <FaGlobe />
+             <FaGlobe  size={30} />
            </span>
-        </Link>
+        </a>
 
       </div>
     </div>
