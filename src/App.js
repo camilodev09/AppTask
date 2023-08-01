3f378f140bcdd0d39b5422fc68401defe0f28c7d
camilodev09 <camilobau2021@gmail.com>
2023-07-31 23:46:12 -0500

    actualiz

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div
-      className="w-full min-h-screen pt-4 font-bodyFont bg-gradient-to-t from-purple-800 blue-600 to-purple-300 text-white px-4 flex flex-col gap-10 justify-center items-center rounded-xl
+      className="w-full min-h-screen pt-4 font-bodyFont bg-gradient-to-t from-purple-800 blue-600 to-purple-300 text-white px-4 flex flex-col gap-10 justify-center items-center 
        "
     >
      <div className="w-[70px] flex justify-center items-center">
